0442737f936da8628feacbac33fc339d88db3396
Ayush Tamang <tamangayush052@gmail.com>
2025-01-05 02:50:59 +0545

    completed 10 features

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyFinanceHub.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyFinanceHub.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f2ad85b430f8a715faf61710fb7a7e206700e570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f4e5e12a9da8cd2b6e3c2f7048c01ab05c42d4f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
