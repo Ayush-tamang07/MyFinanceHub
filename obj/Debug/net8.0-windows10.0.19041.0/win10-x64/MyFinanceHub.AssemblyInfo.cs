7760510ffe998f96265e0eb5e313f91941de9339
Ayush Tamang <tamangayush052@gmail.com>
2025-01-17 10:10:02 +0545

    completed coursework

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyFinanceHub.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyFinanceHub.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9faa237a91a8c063dcec310aa7a28ca9c39b7d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c4c979fda83f48da87c8bdb7a8805fbcf520ad3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFinanceHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
